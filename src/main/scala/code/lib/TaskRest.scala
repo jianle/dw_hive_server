8372186f0437397efa52394deab75d8e638719d2
Ji ZHANG <jizhang@anjuke.com>
2014-04-22 12:35:19 +0800

    task status

--- src/main/scala/code/lib/TaskRest.scala
+++ src/main/scala/code/lib/TaskRest.scala
@@ -8,6 +8,7 @@ import net.liftweb.common.Loggable
 import net.liftweb.http.S
 import net.liftweb.http.rest.RestHelper
 import net.liftweb.json.JsonDSL._
+import net.liftweb.common.Full
 
 object TaskRest extends RestHelper with Loggable {
 
@@ -31,6 +32,23 @@ object TaskRest extends RestHelper with Loggable {
           ("status", "ok") ~ ("id", task.id.get)
 
         case None => ("status", "error") ~ ("msg", "Query cannot be empty.")
+      }
+
+    }
+
+    case "status" :: taskId JsonGet _ => {
+
+      val task = Task.find(taskId(0).toLong) openOr null
+      if (task == null) {
+        ("status", "error") ~ ("msg", "Task id not found.")
+      } else {
+        val taskStatus = task.status.get match {
+          case Task.STATUS_NEW => "new"
+          case Task.STATUS_RUNNING => "running"
+          case Task.STATUS_OK => "ok"
+          case Task.STATUS_ERROR => "error"
+        }
+        ("status", "ok") ~ ("taskStatus", taskStatus)
       }
 
     }
