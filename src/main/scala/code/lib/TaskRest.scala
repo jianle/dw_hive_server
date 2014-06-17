aaf873e001c831922dbcc13c7f60bfefe017c47c
Ji ZHANG <jizhang@anjuke.com>
2014-06-17 10:13:20 +0800

    fix html parser

--- src/main/scala/code/lib/TaskRest.scala
+++ src/main/scala/code/lib/TaskRest.scala
@@ -12,6 +12,7 @@ import net.liftweb.json.JsonAST._
 import net.liftweb.mapper.ByList
 import java.io.{File, FileInputStream, InputStream, ByteArrayInputStream}
 import net.liftweb.http.StreamingResponse
+import net.liftweb.util.Props
 
 object TaskRest extends RestHelper with Loggable {
 
@@ -127,40 +128,38 @@ object TaskRest extends RestHelper with Loggable {
 
     case "cancel" :: taskId :: Nil JsonGet _ => {
 
-      Task.find(taskId.toLong) match {
+      val task = Task.find(taskId.toLong) openOr null
+      if (task == null) {
+        ("status" -> "error") ~ ("msg" -> "Task id not found.")
+      } else {
 
-        case Full(task) =>
+        val taskStatus = task.status.get
+        if (taskStatus == Task.STATUS_NEW || taskStatus == Task.STATUS_RUNNING) {
+          task.status(Task.STATUS_INTERRUPTED).save
 
-          val taskStatus = task.status.get
-          if (taskStatus == Task.STATUS_NEW || taskStatus == Task.STATUS_RUNNING) {
-            task.status(Task.STATUS_INTERRUPTED).save
+          import dispatch._
+          import dispatch.Defaults._
+          import scala.sys.process._
 
-            import dispatch._
-            import dispatch.Defaults._
-            import scala.sys.process._
+          val jt = Props.get("hadoop.job.tracker") openOr "127.0.0.1:50030"
+          val res = Http(url(s"http://$jt/jobtracker.jsp") OK as.String).map(result => {
+            val ptrn = ">(job_[0-9]{12}_[0-9]+)</a>.*?<td id=\"name_[0-9]+\">\\[([0-9]+)\\]".r
+            result.split("\n")
+                .dropWhile(!_.contains("<h2 id=\"running_jobs\">"))
+                .takeWhile(!_.contains("<hr>"))
+                .map(ptrn.findFirstMatchIn(_))
+                .filter(_.nonEmpty).map(_.get).headOption match {
+                  case Some(m) if m.group(2).toLong == task.id.get =>
+                    logger.info("Kill hadoop job: " + m.group(1))
+                    Seq("hadoop", "job", "-kill", m.group(1)) !
 
-            val res = Http(url("http://10.20.8.71:50030/jobtracker.jsp") OK as.String).map(result => {
-              val ptrn = ">(job_[0-9]{12}_[0-9]+)</a>.*?<td id=\"name_[0-9]+\">\\[([0-9]+)\\]".r
-              result.split("\n").map(_.trim)
-                  .dropWhile(!_.contains("<h2 id=\"running_jobs\">Running Jobs</h2>"))
-                  .takeWhile(!_.contains("<h2 id=\"completed_jobs\">Completed Jobs</h2>"))
-                  .map(ptrn.findFirstMatchIn(_))
-                  .filter(_.nonEmpty).map(_.get).headOption match {
-                    case Some(m) if m.group(2).toLong == task.id.get =>
-                      logger.info("Kill hadoop job: " + m.group(1))
-                      Seq("hadoop", "job", "-kill", m.group(1)) !
-                    case _ => 0
-                  }
-            })
-
-            res()
-          }
-
-          ("status" -> "ok") ~ Nil
-
-        case Empty => ("status" -> "error") ~ ("msg" -> "Task id not found.")
+                  case _ => 0
+                }
+          })
+          res()
+        }
+        ("status" -> "ok") ~ Nil
       }
-
     }
 
   })
